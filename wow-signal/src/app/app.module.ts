e13650f983022fa7f46d1edc7ec5c28685b3cd3e
Markovnikov <alex.j.blasingame@gmail.com>
2021-11-06 15:27:41 -0400

    added api call to open weather now gets temp

--- wow-signal/src/app/app.module.ts
+++ wow-signal/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GearPageComponent } from './gear-page/gear-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GearPageComponent
   ],
   imports: [
     BrowserModule,
---
+++ wow-signal/src/app/gear-page/gear-page.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gear-page',
+  templateUrl: './gear-page.component.html',
+  styleUrls: ['./gear-page.component.css']
+})
+export class GearPageComponent implements OnInit {
+  apiURLp1 = 'http://api.openweathermap.org/data/2.5/weather?zip=';
+  apiURLp2 = ',us&appid=7850af680b8391a67426235c8aeacc9d';
+  zipcode = 47904;
+  WeatherData: any; 
+  localTempK: any; 
+  localTempF: any;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.getTempData();
+  }
+
+  getTempData() {
+    fetch(this.apiURLp1 + this.zipcode + this.apiURLp2)
+    .then(response=>response.json())
+    .then(data=>{this.setTempData(data);})
+  }
+
+  setTempData(data: any) {
+    this.WeatherData = data;
+    console.log(this.WeatherData);
+    this.localTempK = this.WeatherData.main.temp;
+    console.log(this.localTempK);
+
+    this.localTempF = (this.localTempK - 273.15) * (9/5) + 32;
+    console.log(this.localTempF);
+
+  }
+
+}
